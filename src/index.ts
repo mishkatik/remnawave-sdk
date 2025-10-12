8e21aedcdce9eceb188338db9a9ba82e29f1b6ac
mishkatik <mishkat@riseup.net>
2025-10-12 19:58:36 +0300

    chore: bump version to 1.1.2, export additional types and interfaces in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export * from '@remnawave/backend-contract';
 export * from './clients';
+export type { Config } from './config';
 export * from './controllers';
+export * from './interfaces';
 export * from './remnawave.sdk';
